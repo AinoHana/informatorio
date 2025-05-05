5f7318770b588b42c56f24799f0cfd70d8af75a8
AinoHana <rociosoledadromano@gmail.com>
2025-05-05 16:50:11 -0300

    primer commit

---
+++ 20250430-estructuras-control.py
@@ -0,0 +1,37 @@
+condicion = True
+condicion_2 = True
+
+#condicional simple
+if condicion:
+    print('se cumple condicion')
+
+#condicional alternativo
+if condicion:
+    print('se cumple condicion')
+
+#condicional multiple
+if condicion:
+    print('se cumple condicion')
+elif(condicion_2):
+    print('se cumple condicion 2')
+else:
+    print('ninguna condicion se cumple')
+
+#Iterativos
+variables = [11,22,33,44,55]
+for x in variables:
+    print('Estoy en el elemento', x)
+
+for elemento in range(10):
+    print('Estoy en el elemento', elemento)
+
+#Cortamos el proceso con ctrl + c
+# while True:
+#     print('sigo ejecutando...')
+
+# contador = 0
+# while (contador != 5):
+#     print('contador vale:', contador)
+# contador = contador + 1    
+ 
+          
---
+++ 20250430-variables-tipo-dato.py
@@ -0,0 +1,68 @@
+int_1 = 5 
+int_2 = 2
+
+#suma
+suma = int_1 + int_2
+print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+#resta
+resta = int_1 - int_2
+print (f"La resta entre {int_1} y {int_2} es: {resta}")
+
+#multiplicacion
+multiplicacion = int_1 + int_2
+print(f"La multiplicacion entre {int_1} y {int_2} es: {multiplicacion}")
+
+# ctrl + k+ c comenta el codigo y ctrl + k + u descomenta
+
+#division
+division = int_1 / int_2
+print(f"La division entre {int_1} y {int_2} es: {division}")
+
+#potencia
+potencia = int_1 + int_2
+print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+##variables string
+str_1 = "info"
+str_2 = "rtorio"
+
+##operadores
+# suma
+suma = str_1 + str_2
+print(f"La suma entre {str_1} y {str_2} es: {suma}")
+
+## variables listas
+lis_1 = [1, 2, 3]
+lis_2 = [4, 5, 6]
+
+# Operadores
+# suma
+suma = lis_1 + lis_2
+print(f"La suma entre {lis_1} y {lis_2} es: {suma}")
+
+# #resta - NO SE PUEDE RESTAR
+# resta = lis_1 - lis_2
+# print(f"La resta entre {lis_1} y {lis_2} es: {resta}")
+
+lista = [93, 12383, 203, 21982, 38747.23, 123, 23, 1, 1, 23, 4]
+#utilizando .sort()
+lista.sort()
+
+print(lista)
+
+# los elementos se ubican con 0 1 2 3...., -1 me da el ultimo elemento
+
+lista_2 = [10,20,30,40,50]
+print(lista_2[-1])
+
+lista_3 = ['Gerardo', 'Andres', 'Ainara']
+lista_3.sort(reverse=True)
+print(lista_3)
+
+#tupla es como lista, pero sí puede imprimir palabras y números, las listas no 
+tupla = ('Gerardo', 'Andres', 'Ainara', 3, 1)
+print(tupla)
+
+diccionario = {'42039192': 'Pablo Perez', '42039193': 'Pablo Pereson'}
+print(diccionario['42039192'])
